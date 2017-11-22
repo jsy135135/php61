c1a42a0bf905291008ad9e0d948f41002ea98b93
jsy135135 <732677288@qq.com>
2017-11-22 20:19:55 +0800

    Signed-off-by: jsy135135 <732677288@qq.com> 开发了333

--- strpos.php
+++ strpos.php
@@ -5,10 +5,11 @@
  * @email:732677288@qq.com
  * @Date:   2017-11-20 14:56:58
  * @Last Modified by:   jsy135135
- * @Last Modified time: 2017-11-22 20:14:31
+ * @Last Modified time: 2017-11-22 20:19:45
  */
 $str = 'abc';
 $rs = strpos($str, 'd');
 var_dump($rs);
 echo 111;
 echo 222;
+echo 333;
